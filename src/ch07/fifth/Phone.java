0faf18cb0dfa2e28f9748cc910e6b6c1531684db
sei-park <tpdlqkr@naver.com>
2024-08-10 18:23:19 +0900

    1

--- src/ch07/fifth/Phone.java
+++ src/ch07/fifth/Phone.java
@@ -10,7 +10,17 @@ public abstract class Phone {
 		System.out.println("turnOff");
 	}
 	
-	public abstract void open();
+	// 추상 메소드가 하나 이상 포함되면 추상클래스로 선언 되어야 함
+	// 추상클래스에서 추상 메소드가 아닌 일반 메소드 포함 가능 
+	
+	// 추상클래스 안의 메소드를 미완성으로 남겨 놓는 이유는 
+	// 추상클래스를 상속 받는 자식클래스의 주제에 따라 
+	// 상속 받는 메소드의 내용이 달라질 수 있기 때문
+	// 실제 내용은 상속 받는 클래스에서 구현하도록 하기 위해 일부러 비워둠
+	
+	public abstract void open(); // 추상 메소드 
+	
+
 	
 	
 
--- src/ch07/fifth/PhoneEx.java
+++ src/ch07/fifth/PhoneEx.java
@@ -4,13 +4,18 @@ public class PhoneEx extends Phone {
 
 	@Override
 	public void open() {
-		// TODO Auto-generated method stub
-		
+		System.out.println("Hello");
 	}
+	
+	// 추상클래스를 상속하는 자식 클래스에서 
+	// 부모가 가진 메소드를 모두 오버라이딩 해야 
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+		
+		PhoneEx phoneEx = new PhoneEx();
+		
+		phoneEx.open();
+	} 
+	
 
 }
